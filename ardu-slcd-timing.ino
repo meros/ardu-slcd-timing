cf6552abe35c0d92700d711ce9af04a0d89af87e
Alexander Schrab <alexander.schrab@gmail.com>
2015-01-03 17:52:47 +0100

    Test 2

--- ardu-slcd-timing.ino
+++ ardu-slcd-timing.ino
@@ -5,19 +5,19 @@
 
 SerialLCD slcd(1,2);
 int samplecount = 0;
-int samplesize = 25;
+int samplesize = 100;
 
 int last = 0;
 
 void setup() {
   slcd.begin();
-  last = micros();
+  last = millis();
 }
 
 void loop(){  
+  slcd.setCursor(0,0);
   for (int y = 0; y < 1; y++) {
     for (int x = 0; x < 16; x++) {
-      slcd.setCursor(x,y);
       slcd.print("-");
     }
   }
@@ -25,14 +25,14 @@ void loop(){
   samplecount++;
   if (samplecount > samplesize) {
     samplecount = 0;
-    int current = micros();
+    int current = millis();
     
     slcd.setCursor(0,1);
     slcd.print("                ");
     slcd.setCursor(0,1);
-    slcd.print((current-last)/samplesize,10);
+    slcd.print((current-last),10);
     
-    last = micros();
+    last = millis();
   }
 }
 
